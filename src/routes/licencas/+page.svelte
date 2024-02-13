642a6b84352e98b51309774c83964e90df9104a5
Cláudio Pereira <git@claudiop.com>
2024-02-13 07:27:21 +0000

    Page with used licenses

---
+++ src/routes/licencas/+page.svelte
@@ -0,0 +1,87 @@
+<!-- Intermodal, transportation information aggregator
+    Copyright (C) 2022  Cláudio Pereira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
+
+<div class="card bg-base-100 w-full shadow-sm gap-4 px-2 lg:px-0">
+	<div class="card-body">
+		<h2 class="card-title">Licenças</h2>
+		<p>
+			O código fonte deste website, do editor e do código que os suporta é disponibilizado sob a
+			licença <a class="link" href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank"
+				>AGPL 3.0</a
+			>.
+		</p>
+		<p>
+			Estamos nos ombros de gigantes. Todo o projeto assenta em recursos artísticos e software livre
+			feito por diversos membros da comunidade.
+		</p>
+		<h2 class="card-title">Informação</h2>
+		<p>
+			A informação a respeito dos serviços de mobilidade foi levantada exclusivamente por nós e como
+			tal detemos plena autoria. Caso queira usar esta informação contacte-nos para lhe concedermos
+			uma licença nos conteúdos desejados (provavelmente
+			<a class="link" href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank"
+				>CC BY-SA</a
+			>).
+		</p>
+		<p>
+			Esta restrição é temporária e existe pelo simples motivo que ainda não refletimos o suficiente
+			sobre a licença a usar.
+		</p>
+		<h2 class="card-title">Artigos e demais produções textuais</h2>
+		<p>
+			Regemos estes conteúdos nos mesmos moldes que a informação. Contacte-nos para obter uma
+			licença de utilização.
+		</p>
+		<p>
+			Frisamos que nem todos os conteúdos utilizados são da nossa autoria, sendo conteúdo de
+			terceiros discrimindos <i>in situ</i>.
+		</p>
+		<h2 class="card-title">Ilustrações</h2>
+		<p>
+			Algumas das ilustrações utilizadas não são da nossa autoria. A licença de cada ilustração pode
+			ser encontrada no código fonte do projeto.
+		</p>
+		<p>
+			Destacam-se os logótipos das operadoras de transporte que são propriedade das mesmas. Fazemos
+			uso dos logótipos afim de identificar e educar para os serviços de transporte. Não detemos
+			qualquer direito sobre os mesmos alem do conferido pela lei.
+		</p>
+		<h2 class="card-title">Cartografia</h2>
+		<p>
+			Os mapas exibidos são tendencialmente projeções personalizadas do <a
+				class="link"
+				href="https://openstreetmap.org"
+				target="_blank">OpenStreetMap</a
+			>. A licença o OpenStreetMap é a
+			<a class="link" href="https://opendatacommons.org/licenses/odbl/">ODbL</a>.
+		</p>
+		<p>
+			Também incorporamos a Carta Administrativa da Direção Geral do Território, que é
+			disponibilizada sob a licença <a
+				class="link"
+				href="https://dados.gov.pt/pt/datasets/carta-administrativa/"
+				target="_blank">CC BY 4.0</a
+			>.
+		</p>
+		<h2 class="card-title">Fontes</h2>
+		<p>
+			Utilizamos a fonte Roboto de Christian Robertson a qual se encontra licenciada com uma licença <a
+				class="link"
+				href="http://www.apache.org/licenses/LICENSE-2.0">Apache 2.0</a
+			>.
+		</p>
+	</div>
+</div>
