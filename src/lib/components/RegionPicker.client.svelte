6a54eee05aa4faeb8092493d656d678333b6e98d
Cláudio Pereira <git@claudiop.com>
2024-02-13 14:52:20 +0000

    Redo mobile navigation

---
+++ src/lib/components/RegionPicker.client.svelte
@@ -0,0 +1,324 @@
+<!-- Intermodal, transportation information aggregator
+    Copyright (C) 2024  Cláudio Pereira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
+<script>
+	import { onMount, onDestroy } from 'svelte';
+	import { derived, writable } from 'svelte/store';
+	import { Map as Maplibre, NavigationControl, GeolocateControl } from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import { liveQuery } from 'dexie';
+	import { fetchRegions, getRegions, regionId, setRegion, loadMissing } from '$lib/db';
+	import { tileStyle } from '$lib/settings.js';
+
+	let map;
+	let mapElem;
+	let regionConfirmationModal;
+	let mapGeolocateControl;
+	let isGeolocating = false;
+
+	let mapLoaded = false;
+	let regionsLoaded = false;
+
+	let hoveredRegionId = null;
+	let explicitRegionChange = false;
+
+	const DEFAULT_BOUNDS = [
+		[-11, 36.5],
+		[-5.5, 42.35]
+	];
+
+	const regions = liveQuery(() => getRegions());
+	const pendingRegionId = writable();
+
+	const pendingRegion = derived([regions, pendingRegionId], ([$regions, $pendingRegionId]) => {
+		if (!$pendingRegionId || !$regions) {
+			return null;
+		}
+		return $regions.find((r) => r.id == $pendingRegionId);
+	});
+	const selectedRegion = derived([regions, regionId], ([$regions, $regionId]) => {
+		console.log('$regionId', $regionId);
+		if (!$regionId || !$regions) {
+			console.log('1');
+			return null;
+		}
+		console.log('2');
+		return $regions.find((r) => r.id == $regionId);
+	});
+
+	regions.subscribe((regs) => {
+		drawRegions();
+	});
+
+	pendingRegion.subscribe((region) => {
+		if (!region) {
+			return;
+		}
+		regionConfirmationModal.showModal();
+	});
+
+	async function confirmPendingRegion() {
+		await setRegion($pendingRegionId);
+		explicitRegionChange = false;
+		$pendingRegionId = null;
+	}
+
+	function drawRegions() {
+		if (!mapLoaded || !$regions) {
+			return;
+		}
+		map.getSource('regions').setData({
+			type: 'FeatureCollection',
+			features: $regions.map((region) => ({
+				type: 'Feature',
+				id: region.id,
+				properties: {
+					id: region.id,
+					name: region.name
+				},
+				geometry: region.geometry
+			}))
+		});
+	}
+
+	async function loadData() {
+		await Promise.all([
+			fetchRegions().then((r) => {
+				regionsLoaded = true;
+				drawRegions();
+				return r;
+			})
+			// fetch(`${apiServer}/v1/news&stuff`)
+			// 	.then((r) => r.json())
+			// 	.then((r) => {
+			// 		data = r;
+			// 	})
+		]);
+	}
+	loadData().then(async () => {
+		console.log('data loaded');
+		await loadMissing();
+	});
+
+	const regionTabs = {
+		north: 1,
+		center: 2,
+		south: 3
+	};
+	let regionTab = writable();
+
+	regionTab.subscribe((tab) => {
+		switch (tab) {
+			case regionTabs.north:
+				// map.setMaxBounds([
+				// 	[-9.14, 40.44],
+				// 	[-7.95, 42.38]
+				// ]);
+				map.flyTo({
+					center: [-8.62, 41.16],
+					zoom: 7
+				});
+				break;
+			case regionTabs.center:
+				map.setMaxBounds(DEFAULT_BOUNDS);
+				map.flyTo({
+					center: [-9.12, 38.72],
+					zoom: 7
+				});
+				break;
+			case regionTabs.south:
+				map.setMaxBounds(DEFAULT_BOUNDS);
+				map.flyTo({
+					center: [-8.0, 37.14],
+					zoom: 8
+				});
+				break;
+			default:
+				if (map) {
+					map.setMaxBounds(DEFAULT_BOUNDS);
+				}
+		}
+	});
+
+	function addSourcesAndLayers() {
+		map.addSource('regions', {
+			type: 'geojson',
+			data: {
+				type: 'FeatureCollection',
+				features: []
+			}
+		});
+
+		map.addLayer({
+			id: 'regions-fills',
+			type: 'fill',
+			source: 'regions',
+			layout: {},
+			paint: {
+				'fill-color': '#627BC1',
+				'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.2, 0.1]
+			}
+		});
+
+		map.addLayer({
+			id: 'region-borders',
+			type: 'line',
+			source: 'regions',
+			layout: {},
+			paint: {
+				'line-color': '#627BC1',
+				'line-width': 0.5
+			}
+		});
+	}
+
+	function addEvents() {
+		mapGeolocateControl.on('geolocate', (e) => {
+			isGeolocating = false;
+			$regionTab = -1;
+		});
+
+		mapGeolocateControl.on('outofmaxbounds', function () {
+			alert('Aparenta estar fora de Portugal continental.');
+			isGeolocating = false;
+		});
+
+		mapGeolocateControl.on('error', function () {
+			alert('Incapaz de obter a sua localização.');
+			isGeolocating = false;
+		});
+
+		map.on('click', 'regions-fills', (e) => {
+			$pendingRegionId = e.features[0].id;
+		});
+
+		map.on('mousemove', 'regions-fills', (e) => {
+			if (e.features.length > 0) {
+				if (hoveredRegionId) {
+					map.setFeatureState({ source: 'regions', id: hoveredRegionId }, { hover: false });
+				}
+				hoveredRegionId = e.features[0].id;
+				map.setFeatureState({ source: 'regions', id: hoveredRegionId }, { hover: true });
+			}
+		});
+
+		map.on('mouseleave', 'regions-fills', () => {
+			if (hoveredRegionId) {
+				map.setFeatureState({ source: 'regions', id: hoveredRegionId }, { hover: false });
+			}
+			hoveredRegionId = null;
+		});
+	}
+
+	onMount(() => {
+		map = new Maplibre({
+			container: mapElem,
+			style: tileStyle,
+			minZoom: 3,
+			zoom: 3,
+			maxZoom: 11,
+			maxBounds: DEFAULT_BOUNDS,
+			center: [-9.1333, 38.7167]
+		});
+
+		mapGeolocateControl = new GeolocateControl();
+		map.addControl(new NavigationControl(), 'top-right');
+		map.addControl(mapGeolocateControl, 'top-right');
+
+		map.on('load', () => {
+			addSourcesAndLayers();
+			addEvents();
+			mapLoaded = true;
+			if (regionsLoaded) {
+				drawRegions();
+			}
+		});
+	});
+
+	onDestroy(() => {
+		mapLoaded = false;
+		map.remove();
+	});
+</script>
+
+<div>
+	<div role="tablist" class="tabs tabs-lg tabs-bordered justify-start" class:hidden={!$regionTab}>
+		<button role="tab" class="tab" on:click={() => ($regionTab = null)}>Voltar</button>
+	</div>
+	<div role="tablist" class="tabs tabs-lg tabs-bordered" class:hidden={$regionTab}>
+		<button
+			role="tab"
+			class="tab"
+			class:tab-active={$regionTab == regionTabs.north}
+			on:click={() => ($regionTab = regionTabs.north)}>Norte</button
+		>
+		<button
+			role="tab"
+			class="tab"
+			class:tab-active={$regionTab == regionTabs.center}
+			on:click={() => ($regionTab = regionTabs.center)}>Centro</button
+		>
+		<button
+			role="tab"
+			class="tab"
+			class:tab-active={$regionTab == regionTabs.south}
+			on:click={() => ($regionTab = regionTabs.south)}>Sul</button
+		>
+	</div>
+	<div
+		bind:this={mapElem}
+		class="w-full h-[50vh] rounded-b-lg relative"
+		class:rounded-tr-lg={$regionTab}
+	>
+		<div
+			class="absolute top-0 w-full h-full z-10 flex flex-col gap-4 justify-center items-center backdrop-blur-md"
+			class:hidden={$regionTab}
+		>
+			{#if isGeolocating}
+				<span class="text-3xl text-neutral">A obter a sua localização</span>
+				<span class="text-lg text-neutral">(pode ter de aceitar)</span>
+				<progress class="progress w-56" />
+			{:else}
+				<span class="text-3xl text-neutral">Escolha a zona do país</span>
+				<button
+					class="btn btn-primary"
+					class:hidden={isGeolocating}
+					on:click={() => {
+						isGeolocating = true;
+						mapGeolocateControl.trigger();
+					}}>Utilizar a minha localização</button
+				>
+			{/if}
+		</div>
+	</div>
+</div>
+<dialog bind:this={regionConfirmationModal} class="modal modal-bottom sm:modal-middle">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg">Escolher <b>{$pendingRegion?.name || ''}</b> como região?</h3>
+		<p class="py-4">
+			Esta definição serve para mostrar os serviços relevantes para a sua região. Pode alterar em
+			qualquer momento a partir do menu.
+		</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn btn-success" on:click={confirmPendingRegion}>Sim</button>
+				<button class="btn" on:click={() => ($pendingRegionId = null)}>Não</button>
+			</form>
+		</div>
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>Fechar</button>
+	</form>
+</dialog>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <!-- Intermodal, transportation information aggregator
-    Copyright (C) 2022  Cláudio Pereira
+    Copyright (C) 2024  Cláudio Pereira
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -15,18 +15,142 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 <script>
 	import { page } from '$app/stores';
-	import Menu from '$lib/components/Menu.svelte';
+	import { selectedRegion } from '$lib/db';
+	import RegionPicker from '$lib/components/RegionPicker.client.svelte';
+	import globe from '$lib/images/globe.svg';
 	import '../app.css';
+
+	let regDialog;
+	let renderDialogMap = false;
+
+	function closeDrawer() {
+		document.getElementById('drawer').checked = false;
+	}
 </script>
 
+<div class="drawer grow">
+	<input id="drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<div
+			class="w-[min(960px,100%)] mx-auto p-0 sm:p-2 pb-4 lg:px-0 xl:pt-4 self-center"
+			class:absolute={$page.data.floatingMenu || false}
+			class:top-0={$page.data.floatingMenu || false}
+			class:z-[3000]={$page.data.floatingMenu || false}
+		>
+			<div
+				class="navbar bg-base-100 sm:rounded-xl"
+				class:shadow-md={$page.data.floatingMenu}
+				class:shadow-sm={!$page.data.floatingMenu}
+			>
+				<div class="navbar-start">
+					<div>
+						<label for="drawer" class="btn btn-ghost drawer-button lg:hidden">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M4 6h16M4 12h8m-8 6h16"
+								/>
+							</svg>
+						</label>
+					</div>
+					<a class="btn btn-ghost hover:bg-zinc-100" href="/">
+						<img src="/logo-text.svg" class="h-8 max-w-[60vw]" alt="Início,Logotipo" />
+					</a>
+				</div>
+				<div class="navbar-end hidden lg:flex">
+					<ul class="menu menu-horizontal p-0 gap-3">
+						{#if $selectedRegion}
+							<li>
+								<a href="/rede" class="p-3" class:active={$page.url.pathname.startsWith('/rede')}
+									>Mapa</a
+								>
+							</li>
+						{/if}
+						<li>
+							<a href="/regiao" class="p-3" class:active={$page.url.pathname.startsWith('/regiao')}
+								>Região</a
+							>
+						</li>
+						<li>
+							<a href="/edu" class="p-3" class:active={$page.url.pathname.startsWith('/edu')}>
+								Educação
+							</a>
+						</li>
+						<li>
+							<button
+								class="bg-base-200 border h-full flex"
+								aria-label="Escolher região"
+								on:click={() => {
+									regDialog.showModal();
+									renderDialogMap = true;
+								}}
+							>
+								<img src={globe} alt="Escolher região" />
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
-<div
-	class="w-[min(960px,100%)] mx-auto p-2 pb-4 xl:pt-4   self-center"
-    class:absolute={$page.data.floatingMenu || false}
-    class:top-0={$page.data.floatingMenu || false}
-    class:z-[3000]={$page.data.floatingMenu || false}
->
-	<Menu />
+		<slot />
+	</div>
+	<div class="drawer-side z-[10000]">
+		<label for="drawer" aria-label="close sidebar" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+			<li><a href="/rede" on:click={closeDrawer} on:keypress={closeDrawer}>Mapa</a></li>
+			<li><a href="/regiao" on:click={closeDrawer} on:keypress={closeDrawer}>Região</a></li>
+			<li><a href="/edu" on:click={closeDrawer} on:keypress={closeDrawer}>Educação</a></li>
+			<span class="grow" />
+			<button
+				class="flex flex-col bg-base-300 hover:bg-base-100 p-4 rounded-xl mb-3"
+				on:click={() => {
+					regDialog.showModal();
+					renderDialogMap = true;
+				}}
+			>
+				<div class="flex gap-2">
+					<img src={globe} alt="Escolher região" />
+					<span>Região</span>
+				</div>
+				{#if $selectedRegion}
+					<span class="font-bold">{$selectedRegion?.name}</span>
+				{:else}
+					<span class="font-bold">Sem região escolhida</span>
+				{/if}
+			</button>
+
+			<label for="drawer" class="btn btn-error"> Fechar menu </label>
+		</ul>
+	</div>
 </div>
-
-<slot />
+<dialog
+	id="regdialog"
+	bind:this={regDialog}
+	class="modal modal-bottom sm:modal-middle"
+	on:close={() => {
+		renderDialogMap = false;
+	}}
+>
+	<div class="modal-box !max-w-[60em]">
+		<h2 class="text-lg font-bold text-center">Escolha a sua região</h2>
+		{#if renderDialogMap}
+			<RegionPicker />
+		{/if}
+		<!-- <div class="flex justify-between max-w-64">
+			<span class="text-lg font-bold">Lingua</span>
+			<span class="font-bold">[Português]</span>
+		</div> -->
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
